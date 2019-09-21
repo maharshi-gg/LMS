ef08c34662162e2e7fb926363bde6f3ba3456627
maharshi-gg <maharshig.parekh2014@gmail.com>
2019-09-21 19:20:23 -0400

    Ported project from sqlite3 to postgres for Heroku compatibility.

--- db/schema.rb
+++ db/schema.rb
@@ -12,6 +12,9 @@
 
 ActiveRecord::Schema.define(version: 2019_09_21_193116) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "admins", force: :cascade do |t|
     t.string "email"
     t.string "name"
