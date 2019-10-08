dfef9127bb17b6fa4fc97f649587208d885b5062
maharshi-gg <maharshig.parekh2014@gmail.com>
2019-10-08 10:02:44 -0400

    Sign Up bug

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -13,7 +13,7 @@ class RegistrationsController < Devise::RegistrationsController
                             university: params[:user][:student][:university], max_books: params[:user][:student][:max_books])
         @stud.save
         super and return
-        redirect_to :student
+        redirect_to :student and return
       else
         # @stud.update(student_signup_params)
         # format.html { redirect_to students_path, notice: 'Student was successfully created.' }
@@ -26,7 +26,7 @@ class RegistrationsController < Devise::RegistrationsController
                              password: sign_up_params[:password], libraries_id: params[:user][:librarian][:libraries_id],
                              approved: false)
         @lib.save
-        redirect_to new_user_session_path, :flash=>{:alert => 'Request has been sent. Pending admin approval.'}
+        redirect_to new_user_session_path, :flash=>{:alert => 'Request has been sent. Pending admin approval.'} and return
         # format.json { render :show, status: :accepted, location: home_index_path}
       else
         # @lib.update(librarian_signup_parmas)
